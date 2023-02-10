cff1695454990832e7985744bddd834e37042940
HacKairos <joao.brasilpv@gmail.com>
2023-02-10 14:37:33 -0300

    Explicação primeiro Problema

--- Baralho.c
+++ Baralho.c
@@ -1,71 +1,71 @@
 #include <stdio.h>
 #include <string.h>
 
-void contador(int *Entrada)
+void contador(int *Entrada) // função para contar a quantidade de cartas que faltam em cada naipe
 {
-    int error = -1, cont = 13;
-    for (int i = 0; i < 13; i++)
+    int error = -1, cont = 13;   // adiciono a variavel que printa o erro e uma com a quantidade maxima de cartas
+    for (int i = 0; i < 13; i++) // verifico em todas as posições do array o valor dentro de suas casas
     {
         if (Entrada[i] == 0)
         {
-            cont--;
+            cont--; // quando o valor é 0 significa que ele apareceu dentro da sting entao o contador diminui pois esta carta nao esta faltando
         }
         else
         {
             if (Entrada[i] < 0)
             {
-                error++;
+                error++; // quando o valor é menor q 0 significa que ela pareceu mais de uma vez dentro da string
             }
         }
     }
     if (error >= 0)
     {
-        printf("erro\n");
+        printf("erro\n"); // printa erro quando ela repete dentro da string
     }
     else
     {
-        printf("%d\n", cont);
+        printf("%d\n", cont); // printa as quantidade que falta em determinado naipe
     }
 }
 
 int main()
 {
-    int C[13], E[13], U[13], P[13];
+    int C[13], E[13], U[13], P[13]; // criação das variaveis
     char entrada[157];
-    for (int i = 0; i < 13; i++)
+    for (int i = 0; i < 13; i++) // preencho todos os arrays de inteiro com o numero 1
     {
         C[i] = 1;
         E[i] = 1;
         U[i] = 1;
         P[i] = 1;
     }
-    scanf(" %[^\n]s", entrada);
-    for (int i = 0; i < strlen(entrada); i = i + 3)
+    scanf(" %[^\n]s", entrada);                     // leitura da entrada
+    for (int i = 0; i < strlen(entrada); i = i + 3) // Passar por toda string com i sempre pulando 3 casas para ser o primeiro numero de todas as "cartas"
     {
-        int x = i + 1, y = i + 2;
-        if ((entrada[i] - '0') == 0)
+        int x = i + 1, y = i + 2;    // adicono variaveis auxiliares
+        if ((entrada[i] - '0') == 0) // Aqui eu comparo a string na posição i com 0 para determinar se a carta é menor que 10
         {
-            if (entrada[y] == 'C')
+            if (entrada[y] == 'C') // Verifico o naipe da carta
             {
-                C[(entrada[x] - '0') - 1]--;
+                C[(entrada[x] - '0') - 1]--; // aqui eu diminuo em o numero dentro do array de inteiro no determinado naipe para representar se tem ou nao ou tem mais de 1
             }
             else
             {
-                if (entrada[y] == 'E')
+                if (entrada[y] == 'E') // Verifico o naipe da carta
                 {
-                    E[(entrada[x] - '0') - 1]--;
+                    E[(entrada[x] - '0') - 1]--; // aqui eu diminuo em o numero dentro do array de inteiro no determinado naipe para representar se tem ou nao ou tem mais de 1
                 }
                 else
                 {
-                    if (entrada[y] == 'U')
+                    if (entrada[y] == 'U') // Verifico o naipe da carta
                     {
-                        U[(entrada[x] - '0') - 1]--;
+                        U[(entrada[x] - '0') - 1]--; // aqui eu diminuo em o numero dentro do array de inteiro no determinado naipe para representar se tem ou nao ou tem mais de 1
                     }
                     else
                     {
-                        if (entrada[y] == 'P')
+                        if (entrada[y] == 'P') // Verifico o naipe da carta
                         {
-                            P[(entrada[x] - '0') - 1]--;
+                            P[(entrada[x] - '0') - 1]--; // aqui eu diminuo em o numero dentro do array de inteiro no determinado naipe para representar se tem ou nao ou tem mais de 1
                         }
                     }
                 }
@@ -73,29 +73,29 @@ int main()
         }
         else
         {
-            int t = 10 + (entrada[x] - '0');
+            int t = 10 + (entrada[x] - '0'); // Varivel para definir a posição do array de inteiro quando a carta é maior que 1 digito
 
-            if (entrada[y] == 'C')
+            if (entrada[y] == 'C') // Verifico o naipe da carta
             {
-                C[t - 1]--;
+                C[t - 1]--; // aqui eu diminuo em o numero dentro do array de inteiro no determinado naipe para representar se tem ou nao ou tem mais de 1
             }
             else
             {
-                if (entrada[y] == 'E')
+                if (entrada[y] == 'E') // Verifico o naipe da carta
                 {
-                    E[t - 1]--;
+                    E[t - 1]--; // aqui eu diminuo em o numero dentro do array de inteiro no determinado naipe para representar se tem ou nao ou tem mais de 1
                 }
                 else
                 {
-                    if (entrada[y] == 'U')
+                    if (entrada[y] == 'U') // Verifico o naipe da carta
                     {
-                        U[t - 1]--;
+                        U[t - 1]--; // aqui eu diminuo em o numero dentro do array de inteiro no determinado naipe para representar se tem ou nao ou tem mais de 1
                     }
                     else
                     {
-                        if (entrada[y] == 'P')
+                        if (entrada[y] == 'P') // Verifico o naipe da carta
                         {
-                            P[t - 1]--;
+                            P[t - 1]--; // aqui eu diminuo em o numero dentro do array de inteiro no determinado naipe para representar se tem ou nao ou tem mais de 1
                         }
                     }
                 }
@@ -103,10 +103,10 @@ int main()
         }
     }
 
-    contador(C);
-    contador(E);
-    contador(U);
-    contador(P);
+    contador(C); // chama a funçao de contagem
+    contador(E); // chama a funçao de contagem
+    contador(U); // chama a funçao de contagem
+    contador(P); // chama a funçao de contagem
 
     return 0;
 }
--- readme.md
+++ readme.md
@@ -1,15 +1,28 @@
 # João Lucas Pinto Vasconcelos
-Este repositorio foi criado com intuito de resolver alguns problemas e explica-los, para ter a oportunidade de participar do processo seletivo de estagiario na Polícia Federal para trabalhar no Inteligeo - Sistema Integrado de Informações Geoespaciais.
+Este repositório foi criado com intuito de resolver alguns problemas e explicá-los, para ter a oportunidade de participar do processo seletivo de estagiário na Polícia Federal para trabalhar no Inteligeo - Sistema Integrado de Informações Geoespaciais.
 
 ## Introdução
-Todos os problemas resolvidos, foram encontrados na pagina da [OBI](https://olimpiada.ic.unicamp.br/pratique/pu/), como descrito na vaga no [Linkedin](https://www.linkedin.com/feed/update/urn:li:activity:7028742707166519296/?utm_source=share&utm_medium=member_ios), optei por resolver 3 problemas, cada um de uma fase da olimpiada do ano de 2021.
+Todos os problemas resolvidos, foram encontrados na página da [OBI](https://olimpiada.ic.unicamp.br/pratique/pu/), como descrito na vaga no [Linkedin](https://www.linkedin.com/feed/update/urn:li:activity:7028742707166519296/?utm_source=share&utm_medium=member_ios), optei por resolver 3 problemas, cada um de uma fase da olimpíada do ano de 2021.
 
 ## [Problema 1 - Baralho](https://olimpiada.ic.unicamp.br/pratique/ps/2021/f1/baralho/)
-Neste primeiro problema eu tentei realizalo utilizando fundamentalmente logica, apesar de não ser um programa eficiente ele utiliza os recursos mais basicos ao se aprender qualquer linguagem, principalmente a linguagem C, linguagem utilizada em todos os problemas.
-### Passo a Passo
-#### Passo 1
-Primeiramente eu crio uma variavel para cada naipe e a variavel de entrada com caractere a mais que o exigido no problema,
+Neste primeiro problema eu tentei realizá-lo utilizando fundamentalmente logica, apesar de não ser um programa eficiente ele utiliza os recursos mais básicos ao se aprender qualquer linguagem, principalmente a linguagem C, linguagem utilizada em todos os problemas.
 
+### Passo a Passo
+
+#### Passo 1
+Primeiramente eu crio um array de 13(representando cada carta do baralho) inteiros para cada naipe e a variável de entrada a qual recebe uma string contendo até 156 caracteres, porem eu optei por alocar o tamanho de até 157 caracteres, para não ter riscos com o \n ao final de toda string. Após definir as Variáveis eu preenchi todas casas dos arrays de cada naipe com o número 1 e então eu leio a entrada de caracteres.
+
+#### Passo 2
+Após ler todos os caracteres eu crio um laço com a variável i começando na posição 0 e ao final do laço ele incrementando 3 posições, para elar sempre esteja no início de cada "carta" como descrito no problema, dentro do laço crio 2 variáveis a x e a y, com elas representando respectivamente i+1 e i+2, a partir daqui eu separo as cartas que são maiores que 9 das menores ou iguais, utilizando a posição i dentro da string como referência, pois se i=1 a carta é maior que 9.
+
+#### Passo 3 
+Ao separar as menores ou iguais de 9 das maiores eu seleciono qual naipe aquela carta representa, verificando a posição y dentro da string e comparo com todos os caracteres na ordem explicitada no problema, caso encontre o naipe da carta eu pego o número dentro da string menos 1, para representar a posição do número dentro do array de inteiros, explicando melhor o número 1 antes adicionado em cada casa do array de inteiro era para representar a presença a ausência ou o excesso de uma determinada carta de um determinado naipe, portanto quando o número está em 0 representa que essa carta tem dentro do baralho. Essa ultima parte vai ser muito importante para o passo final.
+
+#### Passo 4
+Entao eu basicamente repito o processo quando o i é igual a 1 porem eu crio a variável t que recebe 10 + o número que esta na string na posição x e a única alteração do passo 3 é que a posição do array que eu altero é a de t-1.
+
+#### Passo Final
+Ai ao contabilizar todas as aparições eu chamo a função "contador" que basicamente verifica em todas as posições dos arrays de inteiro se o número reservado é 0, 1 ou -1 e retorna o valor de cartas que faltam em determinados naipes ou se possui cartas repetidas dentro deles representando com a mensagem erro.
 
 
 ## Problema 2 - Lista palíndroma
